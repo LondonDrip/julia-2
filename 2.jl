e1767a5c4131f553a78a27ed8adbef2bd30dbf47
kid <113590185+LondonDrip@users.noreply.github.com>
2023-11-12 00:02:09 +0300

    Delete 2.jl

--- 2.jl
+++
@@ -1,8 +0,0 @@
-function extended_euclidean_algorithm(a, b)
-    if b == 0
-        return a, 1, 0
-    else
-        d, x, y = extended_euclidean_algorithm(b, a % b)
-        return d, y, x - (a ÷ b) * y
-    end
-end
