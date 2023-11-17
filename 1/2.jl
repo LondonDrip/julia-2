14f0a03ae61979182c62a9114f6ddb7fcfc05990
kid <113590185+LondonDrip@users.noreply.github.com>
2023-11-17 14:40:45 +0300

    Update 2.jl

--- 1/2.jl
+++ 1/2.jl
@@ -1,8 +1,8 @@
-function extended_euclidean_algorithm(a, b)
+function extended_euclidean_algorithm(a, b)  #нахождение нод через расширенный алгоритм Евклида.
     if b == 0
-        return a, 1, 0
+        return a, 1, 0    # возвращает значения а,1,0, если b равно 0 
     else
-        d, x, y = extended_euclidean_algorithm(b, a % b)
-        return d, y, x - (a ÷ b) * y
+        d, x, y = extended_euclidean_algorithm(b, a % b) #возвращве значения d,x,y где d это НОД, а х и у это коэффициенты.
+        return d, y, x - (a ÷ b) * y   # это выражение обновляет значения х и у используя свойства коффициентов Безу.
     end
 end
