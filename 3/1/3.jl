9cebcf601bc5ba107e7b508692731ce782d97b4b
kid <113590185+LondonDrip@users.noreply.github.com>
2023-11-17 15:10:35 +0300

    Update 3.jl

--- 3/1/3.jl
+++ 3/1/3.jl
@@ -1,15 +1,15 @@
-function Factorize(n::IntType) where IntType <: Integer
-    list = NamedTuple{(:div, :deg), Tuple{IntType, IntType}}[]
-    for p in eratosphenes_sieve(Int(ceil(n/2)))
-        k = degree(n, p) 
-        if k > 0
+function Factorize(n::IntType) where IntType <: Integer   #   разлагает число n на простые множители и возвращает результат в виде массива именножанных кортежей.
+    list = NamedTuple{(:div, :deg), Tuple{IntType, IntType}}[]   # создает пустой массив который будет содержать именованные кортежи с инфо. о простых множ.
+    for p in eratosphenes_sieve(Int(ceil(n/2)))   #   интерируем объект  по простым числам используя функцию. это позволит интерировать только до числа которое явл. половиной n
+        k = degree(n, p)    #   вычисляет степень p в разложении числа n с использованием финкции degree/
+        if k > 0   #   если степень больше нуля добавляет именованный кортеж в массив.
             push!(list, (div=p, deg=k))
         end
     end
     return list
 end
 
-function degree(n, p) 
+function degree(n, p)    # вычисляет степень простого множителя p в разложении числа n. она считает сколько раз p делит n. возвращается полученная степень k.
     k=0
     n, r = divrem(n,p)
     while n > 0 && r == 0
