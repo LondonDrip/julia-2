602e41c92260b4dfabdb8bcdd064cacae3e555dc
kid <113590185+LondonDrip@users.noreply.github.com>
2023-11-17 15:30:28 +0300

    Update 6.jl

--- 1/6.jl
+++ 1/6.jl
@@ -1,7 +1,7 @@
-struct Polynom{T}
+struct Polynom{T}   # определяет структуру полтном параментризованную типом T.
     coeffs::Vector{T}
 
-    function Polynom(coeffs::Vector{T}) where {T<:Number}
+    function Polynom(coeffs::Vector{T}) where {T<:Number}  # принимает вектор коэф. и создает объект полином. внутри конструктора удаляются ведущие нулевые коэф, чтобы уменьшить размер вектора коэф.
         while length(coeffs) > 1 && coeffs[end] == zero(T)
             pop!(coeffs)
         end
@@ -13,10 +13,10 @@ end
 import Base: +, -, *, show
 
 #Операция +
-+(a::Polynom{T}, b::Polynom{T}) where {T<:Number} = Polynom{T}(a.coeffs + b.coeffs)
++(a::Polynom{T}, b::Polynom{T}) where {T<:Number} = Polynom{T}(a.coeffs + b.coeffs) #выполняется поэлементно
 
 #Операция -
--(a::Polynom{T}, b::Polynom{T}) where {T<:Number} = Polynom{T}(a.coeffs - b.coeffs)
+-(a::Polynom{T}, b::Polynom{T}) where {T<:Number} = Polynom{T}(a.coeffs - b.coeffs)  #выполняется поэлементно
 
 #Унарный -
--(a::Polynom{T}) where {T<:Number} = Polynom{T}(-a.coeffs)
+-(a::Polynom{T}) where {T<:Number} = Polynom{T}(-a.coeffs)  # применяется к каждому коэф полинома.
